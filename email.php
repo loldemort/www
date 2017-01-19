cb0eb5c7f6fa3701501292f051151117e86b3e41
Øystein Hauan <oysteibh@stud.ntnu.no>
2017-01-19 13:04:19 +0100

    trying php server

---
+++ email.php
@@ -0,0 +1,31 @@
+<?php
+require("class.PHPMailer.php");
+
+$mail = new PHPMailer();
+
+$mail->IsSMTP();                                      // set mailer to use SMTP
+$mail->Host = "smtp.domeneshop.no;smtp.domeneshop.no";  // specify main and backup server
+$mail->SMTPAuth = true;     // turn on SMTP authentication
+$mail->Username = "ingridogjoakim1";  // SMTP username
+$mail->Password = "bB=9@tSH"; // SMTP password
+
+$mail->From = "Invitasjon";
+$mail->FromName = "Bryllupweb";
+$mail->AddAddress("oysteinbhauan@gmail.com");                  // name is optional
+
+$mail->WordWrap = 50;                                 // set word wrap to 50 characters
+$mail->IsHTML(true);                                  // set email format to HTML
+
+$mail->Subject = "Here is the subject";
+$mail->Body    = "This is the HTML message body <b>in bold!</b>";
+$mail->AltBody = "This is the body in plain text for non-HTML mail clients";
+
+if(!$mail->Send())
+{
+   echo "Message could not be sent. <p>";
+   echo "Mailer Error: " . $mail->ErrorInfo;
+   exit;
+}
+
+echo "Message has been sent";
+?>
