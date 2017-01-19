1b7ef1161765a65c4b1b6ef7a114e5d041b09245
Øystein Hauan <oysteibh@stud.ntnu.no>
2017-01-19 14:54:24 +0100

    trying php server

--- email.php
+++ email.php
@@ -4,14 +4,14 @@
    $mail = new PHPMailer();
    $mail->IsSMTP();
    $mail->Mailer = "smtp";
-   $mail->Host = "smtp.gmail.com";
+   $mail->Host = "email-smtp.us-east-1.amazonaws.com";
    $mail->SMTPSecure = 'tls';
    $mail->Port = "587"; // 8025, 587 and 25 can also be used. Use Port 465 for SSL.
    $mail->SMTPAuth = true;
    $mail->SMTPDebug = 1;
    $mail->IsHTML(true);
-   $mail->Username = "ingridogjoakim@gmail.com";
-   $mail->Password = "Flanders2";
+   $mail->Username = "AKIAI6QIWAF6XJQT2FEQ";
+   $mail->Password = "AgI86lg6cpg/qntNj+NwShyjl+fxmdnA1Yc+nwXNIjpU";
 
    $mail->From     = "ingridogjoakim@gmail.com";
    $mail->FromName = "Bryllupsweb";
